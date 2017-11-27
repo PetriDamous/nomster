9b3e9d40aace57486927d85769d27382f1d1c485
dwilliams64 <dwilliams_64@yahoo.com>
2017-11-27 23:25:25 +0000

    Created a page that shows more details about places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,10 @@ class PlacesController < ApplicationController
         redirect_to root_path
     end
     
+    def show
+        @places = Place.find(params[:id])
+    end
+    
     private
     
     def place_params
