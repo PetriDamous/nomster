61b9ad01f196d54a750d7acee9ffe92cc7ff4b4d
dwilliams64 <dwilliams_64@yahoo.com>
2017-12-08 23:58:31 +0000

    Started working on commenting system

---
+++ db/migrate/20171208230810_create_comments.rb
@@ -0,0 +1,17 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      #new fields that keep track of messages or comments from users and the rating
+      t.text :message
+      t.string :rating
+      #points to user_id and place_id in other tables
+      t.integer :user_id
+      t.integr :place_id
+      t.timestamps
+    end
+    #speeds up search for both user_id by itself or user_id and place_id together
+    add_index :comments, [:user_id, :place_id]
+    #speeds up search for the place id by its self
+    add_index :comments, :place_id
+  end
+end
