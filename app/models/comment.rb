9e32b9af379a3c1a0f6a623bb5889b2d26d6f26f
dwilliams64 <dwilliams_64@yahoo.com>
2017-12-10 19:56:27 +0000

    Added rating system for places

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+        'one_star': '1_star',
+        'two_star': '2_star',
+        'three_star': '3_star',
+        'four_star': '4_star',
+        'five_star': '5_star'
+    }
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
 end
