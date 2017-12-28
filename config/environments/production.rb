d2453cac42c3c5ff54b86799e0dfa3d2ab5882c4
dwilliams64 <dwilliams_64@yahoo.com>
2017-12-28 01:07:41 +0000

    Setup Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,16 @@ Rails.application.configure do
   
   #Devise
    config.action_mailer.default_url_options = { host: 'nomster-dimitri-williams.herokuapp.com' }
+   
+   #Action Mailer and Sendgrid
+   ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
   
 end
